d6376da5f41df81fc7cb19a748cae8a80ccf5806
Marcelus Mantovani <51h2u3fv@anonaddy.me>
2022-12-10 11:20:38 +0000

    task1-created the 3 modules scafolding.

---
+++ main-api/src/main/java/com/marcelus/securinguserviceexercise/mainapi/Main.java
@@ -0,0 +1,8 @@
+package com.marcelus.securinguserviceexercise.mainapi;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // stub
+    }
+}
---
+++ security/src/main/java/com/marcelus/securinguserviceexercise/security/Main.java
@@ -0,0 +1,8 @@
+package com.marcelus.securinguserviceexercise.security;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // stub
+    }
+}
